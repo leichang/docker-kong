d68015bd898617d4e3536a6e6b9b9a4820e76d16
zwmlzaq <zwmlzaq@users.noreply.github.com>
2017-03-20 15:40:49 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,9 @@ set -e
 # Disabling nginx daemon mode
 export KONG_NGINX_DAEMON="off"
 
+IP_ADDR=`ifconfig eth0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}'`
+echo "SETTING IP_ADDR FOR KONG CLUSTERING TO: ${IP_ADDR}"
+
+export KONG_CLUSTER_LISTEN="${IP_ADDR}:7946"
+
 exec "$@"
