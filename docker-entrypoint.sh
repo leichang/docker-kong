1ffa6e4b66f15821dde923bd947b4bd7ca6c6674
zwmlzaq <zwmlzaq@users.noreply.github.com>
2017-03-30 12:57:58 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 export KONG_NGINX_DAEMON="off"
 
 if [ -z "$DEVICE" ]; then
-  DEVICE="eth2"
+  DEVICE="eth0"
 fi
 
 IP_ADDR=`ifconfig "$DEVICE" | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}'`
